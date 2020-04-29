cff39aa5e7fd79b741283363db828eee714adf60
lisz1012 <lisz1012@163.com>
2020-04-28 21:19:38 -0700

    Add notes and change the 'server-url' to 'service-url'

--- src/main/java/com/lisz/EurekaApplication.java
+++ src/main/java/com/lisz/EurekaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+// 集群启动是要配置program arguments：--spring.profiles.active=7901/7902/7903
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
