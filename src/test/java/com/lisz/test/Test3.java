bd439cf64efc27d14adac6ec8490d7659a9af49e
lisz1012 <lisz1012@163.com>
2020-05-27 16:07:58 -0700

    Runnable without record field

---
+++ src/test/java/com/lisz/test/Test3.java
@@ -0,0 +1,39 @@
+package com.lisz.test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Entity entity = new Entity("e0");
+        Record record = new Record(entity);
+        Runnable r = new TraceRunnable(() -> {
+            System.out.println("Inside runnable entity name: " + record.getEntity().getName()); // underlying.run()中执行
+        }, record);
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.execute(r);
+        service.submit(r); // submit可以允许在自定义线程池里有个返回值
+
+        service.shutdown();
+    }
+
+    private static class TraceRunnable implements Runnable {
+        private final Runnable underlying;
+        //private final Record recorder;
+
+        public TraceRunnable(Runnable underlying, Record recorder) {
+            this.underlying = underlying;
+            //this.recorder = recorder;
+        }
+
+        @Override
+        public void run() {
+        //    System.out.println(recorder.getEntity().getName());
+            underlying.run();
+        }
+    }
+}
+/* 打印结果：
+Inside runnable entity name: e0
+Inside runnable entity name: e0
+ */
