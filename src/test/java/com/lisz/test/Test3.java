7708788b934d2adbd7ae7190a4cde3353b4ff1e7
lisz1012 <lisz1012@163.com>
2020-05-27 20:03:15 -0700

    Add inner class equivalent to lambda

--- src/test/java/com/lisz/test/Test3.java
+++ src/test/java/com/lisz/test/Test3.java
@@ -10,9 +10,21 @@ public class Test3 {
         Runnable r = new TraceRunnable(() -> {
             System.out.println("Inside runnable entity name: " + record.getEntity().getName()); // 这一句会在underlying.run()中执行
         });
+
+        // 下面写法等效于上面的lambda表达式（除了打印的内容），run方法里的内容这里只是"注册"一下，并不立即执行，execute的时候会t.run()回到这里打印
+        // 内部类可以看到外部类的变量，所以这时record在下面的run()里面是可见的，execute的执行路径回到这里的时候也能拿到record
+        class UnderlyingRunnable implements Runnable{
+            @Override
+            public void run() {
+                System.out.println("Inside runnable entity name from UnderlyingRunnable: " + record.getEntity().getName());
+            }
+        }
+        System.out.println("-----我最先打印-----"); // 先于上面的"注册"（run）方法打印
         ExecutorService service = Executors.newCachedThreadPool();
         service.execute(r);
         //service.submit(r);
+        Runnable t = new UnderlyingRunnable();
+        service.execute(t);
 
         service.shutdown();
     }
@@ -28,11 +40,12 @@ public class Test3 {
 
         @Override
         public void run() {
-        //    System.out.println(recorder.getEntity().getName());
-            underlying.run();
+            underlying.run(); // underlying是个内部类的对象，能看到其外部类的record变量
         }
     }
 }
 /* 打印结果：
+-----我最先打印-----
 Inside runnable entity name: e0
+Inside runnable entity name from UnderlyingRunnable: e0
  */
